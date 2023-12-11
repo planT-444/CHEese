3e517832f5c350688bc837b5a326bed76485b6d8
planT-444 <williamhu03@gmail.com>
2023-12-10 21:01:51 -0500

    added legality check

--- components.py
+++ components.py
@@ -24,7 +24,7 @@ test_position = [list(row) for row in test_position_str]
 class Board:
     def __init__(self):
         self.grid = [['0'] * 8 for i in range(8)]
-        self.castle = {'w': True, 'b': False}
+        self.castle = {'w': True, 'b': True}
         for i in range(8):
             for j in range(8):
                 cur_id = test_position[i][j]
@@ -41,6 +41,7 @@ class Board:
         new_grid = [[p.copy() for p in row] for row in self.grid]
         new_board = Board()
         new_board.grid = new_grid
+        new_board.castle = self.castle.copy()
         return new_board
     
     def __str__(self):
@@ -78,8 +79,118 @@ class Piece:
         return piece_type(id, color, row, col, board)
 
 class Move:
-    def __init__(self, piece_updates: list[list[tuple[Piece, int, int]]], check_legal = None):
-        check_legal = check_legal or piece_updates
+    def __init__(self, board, piece, piece_update:tuple[int, int, int, int], special = None):
+        self.board = board
+        self.piece = piece
+        self.piece_update = piece_update
+        # choices for special: enpassant, castle, promotion (format =[id])
+        self.special = special
+        
+
+    # returns boolean, modified board object, move id
+    def mega_move_func(self):
+        if self.piece.id.lower() in 'rk':
+            self.board.castle[self.piece.color] = False
+        
+        board_grid = self.board.grid
+        board_grid_copy = board_grid.copy()
+        old_row, old_col, new_row, new_col = self.piece_update
+
+        # easy way out, changing the self.piece to the promoted one
+        if self.special is not None and self.special[0] == '=':
+            self.piece = Piece.create_piece(self.special[1:], new_row, new_col, self.board)
+
+        board_grid[new_row][new_col] = self.piece
+        self.piece.row = new_row
+        self.piece.col = new_col
+
+
+        if self.special is not None:
+            if self.special == 'enpassant':
+                captured_row = new_row + (1 if self.piece.color == 'w' else -1)
+                board_grid[captured_row][new_col] = Piece.create_piece('_', captured_row, new_col, self.board)
+                move_id = f'{Board.coords(0, old_col)[1]}x{Board.coords(new_row, new_col)} e.p.'
+
+            elif self.special == 'castle':
+                # queenside else kingside
+                old_rook_col, new_rook_col, move_id = (0, 3, 'O-O-O') if new_col == 2 else (7, 5, 'O-O') 
+                board_grid[new_row][old_rook_col] = Piece.create_piece('_', new_row, old_rook_col, self.board)
+                new_rook_id = 'R' if self.piece.color == 'w' else 'r'
+                board_grid[new_row][new_rook_col] = Piece.create_piece(new_rook_id, new_row, new_rook_col, self.board)
+            else: # promotion
+                move_id = f'{Board.coords(new_row, new_col)}={self.piece.id.upper()}'
+            
+            return True, self.board, move_id
+
+
+        king = None
+        for row in range(8):
+            for col in range(8):
+                cur_piece = board_grid[row][col]
+                if cur_piece.id.lower() == 'k' and cur_piece.color == self.piece.color:
+                    king = cur_piece
+                    break
+            if king is not None:
+                break
+        if king.is_incheck():
+            return False, None, None
+
+
+        old_coords = Board.coords(old_row, old_col)
+        new_coords = Board.coords(new_row, new_col)
+        capture = '' if board_grid_copy[new_row][new_col].id == '_' else 'x'
+        match self.piece.id.lower():
+            case 'p':
+                move_id = new_coords if capture == '' else f'{old_coords[1]}x{new_coords}'
+            case 'k':
+                move_id = f'K{capture}{new_coords}'
+            case _:
+                search_moves = {
+                    "rq": ((1,0),(-1,0),(0,1),(0,-1)),
+                    "bq": ((1,1),(1,-1),(-1,1),(-1,-1)),
+                    "n": ((2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)),
+                }
+                board_grid_copy[old_row][old_col] = self.piece
+                look_alike_rows = []
+                look_alike_cols = []
+                for piece_ids in search_moves:
+                    if self.piece.id.lower() not in piece_ids:
+                        continue
+                    cur_row, cur_col = new_row, new_col
+                    for row_move, col_move in search_moves[piece_ids]:
+                        while True:
+                            if not Piece.is_inbounds:
+                                break
+                            cur_square = board_grid_copy[cur_row][cur_col]
+                            if cur_square.id == self.piece.id and (cur_row, cur_col) != (old_row, old_col):
+                                look_alike_rows.append(cur_row)
+                                look_alike_cols.append(cur_col)
+                                break
+                            if cur_square.id != '_':
+                                break
+                
+                row_match = old_row in look_alike_rows
+                col_match = old_col in look_alike_cols
+
+                move_id = self.piece.upper()
+                if row_match and col_match:
+                    move_id += old_coords
+                elif col_match:
+                    move_id += old_coords[1]
+                elif row_match:
+                    move_id += old_coords[0]
+                
+                move_id += capture + new_coords
+                
+
+
+                
+        return True, self.board, move_id
+
+
+
+
+
 
 
 class King(Piece):
@@ -87,8 +198,8 @@ class King(Piece):
         board_grid = self.board.grid
         is_same_color = lambda piece: piece.id != '_' and piece.color == self.color
         search_moves = {
-            "qrk": ((1,0),(-1,0),(0,1),(0,-1)),
-            "qbk": ((1,1),(1,-1),(-1,1),(-1,-1)),
+            "qr": ((1,0),(-1,0),(0,1),(0,-1)),
+            "qb": ((1,1),(1,-1),(-1,1),(-1,-1)),
             "n": ((2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)),
             "p": ((1,1),(1,-1),(-1,1),(-1,-1))
         }
@@ -100,7 +211,7 @@ class King(Piece):
             if self.color == 'b':
                     look_out = look_out.upper()
             
-            if stranger_danger in ('qrk', 'qbk', 'n'):
+            if stranger_danger in ('qr', 'qb', 'n'):
                 # loop to search in different directions
                 for row_move, col_move in search_moves[stranger_danger]:
                     cur_row, cur_col = self.row + row_move, self.col + col_move
